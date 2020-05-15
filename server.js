a485942d3e3f3a059240927e765652adf188c9c6
an-apluss <boscotec.inc@gmail.com>
2020-05-15 22:23:52 +0100

    setup and connect to mongoDB Atlas

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 
 const app = express();
-const PORT = process.env.PORT || "5050";
+const connectDB = require('./config/db');
+
+//connect to database
+connectDB();
 
 app.get("/", (req, res) => res.send("API running..."));
+
+const PORT = process.env.PORT || "5050";
 
 app.listen(PORT, () => console.log(`server started! Listening to port:${PORT}...`));
