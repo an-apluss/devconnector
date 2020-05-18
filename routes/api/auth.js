61e4da9e3f62d7df5eb91c7f1e38f05c4c892262
an-apluss <boscotec.inc@gmail.com>
2020-05-18 12:23:01 +0100

    add endpoint to log in a user

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -24,4 +24,64 @@ router.get("/", auth, async (req, res) => {
   }
 });
 
+router.post(
+  "/",
+  [
+    check("email", "Please provide valid email").isEmail(),
+    check("password", "Password is required").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Authentication Failed: Invalid credential provided" }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Authentication Failed: Invalid credential provided" }] });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+          email: user.email,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtPrivateKey"),
+        { expiresIn: 3600 },
+        (error, token) => {
+          if (error) {
+            throw error;
+          }
+
+          return res.status(200).json({ token });
+        }
+      );
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).json({
+        msg: "Server Error: User cannot be logged in",
+      });
+    }
+  }
+);
+
 module.exports = router;
