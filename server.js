31561ad5104a987fa5733dc1e9f2500cec3ceb74
an-apluss <anuoluwapoakinseye@gmail.com>
2020-06-02 15:30:12 +0100

    configure cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const connectDB = require("./config/db");
@@ -6,15 +7,27 @@ const connectDB = require("./config/db");
 //connect to database
 connectDB();
 
-app.use(express.json({ extended: true }))
+app.use(express.json({ extended: true }));
 
-app.get("/", (req, res) => res.send("API running..."));
+const whitelist = ["http://localhost:3000"];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.get("/", cors(corsOptions), (req, res) => res.send("API running..."));
 
 //define routes
-app.use("/api/v1/users", require("./routes/api/users"));
-app.use("/api/v1/auth", require("./routes/api/auth"));
-app.use("/api/v1/profile", require("./routes/api/profile"));
-app.use("/api/v1/posts", require("./routes/api/posts"));
+app.use("/api/v1/users", cors(corsOptions), require("./routes/api/users"));
+app.use("/api/v1/auth", cors(corsOptions), require("./routes/api/auth"));
+app.use("/api/v1/profile", cors(corsOptions), require("./routes/api/profile"));
+app.use("/api/v1/posts", cors(corsOptions), require("./routes/api/posts"));
 
 const PORT = process.env.PORT || "5050";
 
