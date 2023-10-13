075d47ea284fa65841e43cb513e1003eaeb77000
Anuoluwapo Akinseye <anuoluwapo.akinseye@sterling.ng>
2023-10-13 14:00:02 +0100

    refactor codebase

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -10,8 +10,6 @@ const User = require("../../models/User");
 
 router.get("/", auth, async (req, res) => {
   try {
-    // console.log(req.user)
-    // return
     const user = await User.findById(req.user.id).select("-password");
 
     res.status(200).json({ user });
